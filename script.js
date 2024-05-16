59fabee1553e65d9da343c5594651737e8a2bc3f
vviktoroduvan@gmail.com <vviktoroduvan@gmail.com>
2024-05-16 21:55:36 +0200

    DZ 4.2.2

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 
+
+
 let strike = prompt("Введіть трьохзначне число")
 console.log(strike[0], strike[1], strike[2]);
 console.log(strike.split(''));
-if (strike[0] === strike[1] && strike[1] === strike[2]) {
-    console.log("Так, всі цифри однакові")
+if (strike[0] === strike[1] || strike[1] === strike[2] || strike[0] === strike[0]) {
+    console.log("Так, 2 цифри однакові")
 }
 else{
     console.log("Ні, цифри неоднакові")
