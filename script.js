2dabb597b6080ebd51b84ce669a37fba8cfc80e2
vviktoroduvan@gmail.com <vviktoroduvan@gmail.com>
2024-05-16 22:42:50 +0200

    DZ 4.4

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 
 
+let numOrStr = prompt('input number or string');
+console.log(numOrStr)
 
-let strike = prompt("Введіть трьохзначне число")
-console.log(strike[0], strike[1], strike[2]);
-console.log(strike.split(''));
-if (strike[0] === strike[1] || strike[1] === strike[2] || strike[0] === strike[2]) {
-    console.log("Так, 2 цифри однакові")
+switch (numOrStr) {
+    case "Cansel":
+        result = (numOrStr === null);
+        break;
+    case "Empty String":
+        result = ( numOrStr.trim() === '' );
+        break;
+    case "number is Ba_NaN":
+        result = ( isNaN( +numOrStr ));
+        break;
+        console.log(`Результат:${ result}`);
 }
-else{
-    console.log("Ні, цифри неоднакові")
-}
+
+
