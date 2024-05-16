bc643538b875a45e404ccfd6a23fd73b35fb10b5
vviktoroduvan@gmail.com <vviktoroduvan@gmail.com>
2024-05-16 22:01:38 +0200

    DZ 4.2.2.1

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 let strike = prompt("Введіть трьохзначне число")
 console.log(strike[0], strike[1], strike[2]);
 console.log(strike.split(''));
-if (strike[0] === strike[1] || strike[1] === strike[2] || strike[0] === strike[0]) {
+if (strike[0] === strike[1] || strike[1] === strike[2] || strike[0] === strike[2]) {
     console.log("Так, 2 цифри однакові")
 }
 else{
