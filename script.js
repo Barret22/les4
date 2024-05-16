ebe0857f10fbf5912d7887f863f0103017fa0293
vviktoroduvan@gmail.com <vviktoroduvan@gmail.com>
2024-05-16 05:37:15 +0200

    dz 4.2

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 
-
-let name = prompt("What is your name?");
-if (name !== null) {
-    console.log(`"Hello, ${name}! How are you?"`);
-} else {
-    console.log(" ");
+let strike = prompt("Введіть трьохзначне число")
+console.log(strike[0], strike[1], strike[2]);
+console.log(strike.split(''));
+if (strike[0] === strike[1] && strike[1] === strike[2]) {
+    console.log("Так, всі цифри однакові")
+}
+else{
+    console.log("Ні, цифри неоднакові")
 }
